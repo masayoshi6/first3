dbb132155c5ae9ea7236d9df4ff94fa7826d72bc
masayoshi6 <okumurakiyoshikou@MacBook-Air.local>
2025-03-14 12:20:34 +0900

    ３にした

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 110; i++) {
+        for (int i = 1; i <= 3; i++) {
             //TIP <shortcut actionId="Debug"/> を押してコードのデバッグを開始します。<icon src="AllIcons.Debugger.Db_set_breakpoint"/> ブレークポイントを 1 つ設定しましたが、
             // <shortcut actionId="ToggleLineBreakpoint"/> を押すといつでも他のブレークポイントを追加できます。
             System.out.println("i = " + i);
