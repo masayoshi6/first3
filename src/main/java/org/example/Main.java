26fa17f3acaa63f7f4618b987b6fb82f2b1d3282
masayoshi6 <okumurakiyoshikou@MacBook-Air.local>
2025-05-17 20:45:23 +0900

    forループ修正

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,8 @@ public class Main {
             //TIP <shortcut actionId="Debug"/> を押してコードのデバッグを開始します。<icon src="AllIcons.Debugger.Db_set_breakpoint"/> ブレークポイントを 1 つ設定しましたが、
             // <shortcut actionId="ToggleLineBreakpoint"/> を押すといつでも他のブレークポイントを追加できます。
             System.out.println("i = " + i);
-            System.out.println("こんばんは");
         }
+            System.out.println("こんばんは");
+            System.out.println("座組");
     }
 }
